4b4f3f88e0255cf930301e2c4decb2dc8f66a36c
Okane <anatole.giniaux@sfr.fr>
2023-09-14 18:14:41 -0400

    More permissive code
    Few fix, docstrings and optimization

--- src/fileRenamer.py
+++ src/fileRenamer.py
@@ -9,7 +9,7 @@
 ##############################################################
 
 """
-Initially made to rename the files of a manga chapter downloaded from rawkuma with HakuNeko (because page 2 is a trashy ad). \n
+Initially made to rename the files of manga chapters downloaded from Rawkuma with HakuNeko (because page 2 is a trashy ad). \n
 Use it for whatever you want, if it's legal. Modify it as you want if you want. \n
 
 Author: Okane (Zinnia Scans)
@@ -24,16 +24,16 @@ from time import sleep
 
 # Class
 class FileRenamer():
-    def __renamer(self, path: str, file_extension: str, start: int = 0 , rename_format = "0"):
+    def __renamer(self, path: str, file_extension: str, start_file_name: str, start_number: int, rename_format: str):
         """
-            Rename any file in a folder, starting at a given number (default 0). \n
+            Rename any file in a folder, starting at a given number and avoid dupes. \n
         """
-        i: int = start # No need to rename the files before the start
-        new_name : str = "0"
+        i: int = start_number # No need to rename the files before the start
+        new_name : str = rename_format
 
         for filename in os.listdir(path):
 
-            if (filename != f"0{start-1}{file_extension}"): # No need to rename the file before the start
+            if (filename != f"{start_file_name}{file_extension}"): # No need to rename the file before the start
                 new_name += str(i)
 
                 # File renaming
@@ -51,32 +51,33 @@ class FileRenamer():
 
                 i += 1
             
-    def __remove_n(self, path: str, file_extension: str, start: int = 0 ):
+    def __remove_n(self, path: str, file_extension: str, start_file_name: str):
         """
             Remove the "n_" at the beginning of the file name, added by the rename function (n_01.jpg -> 01.jpg)
         """
         for filename in os.listdir(path):
 
-            if (filename != f"0{start-1}{file_extension}"):
+            if (filename != f"{start_file_name}{file_extension}"):
 
                 original_name = path + filename
                 new_name = path + filename[2:] # Get the original name without the "n_" (basically remove the first two characters)
 
                 os.rename(original_name, new_name)
 
-    def rename(self, path: str, file_extension: str, start: int = 0 , rename_format = "0"):
+    def rename(self, path: str, file_extension: str, start_file_name: str = "00", start_number: int = 0 , rename_format: str = ""):
         """
-            Rename any file in a folder, starting at a given number (default 0). \n
+            Rename every file in a folder, in alphabetical order, starting at a given number (default 0) and a given file (00.file_extension). \n
 
         Args:
             path (str): The path to the folder containing the files to rename
-            start (int, optional): The number of the first file to rename. Defaults to 0. (If you want to start renaming at the second file (02, 03...), put 2)
+            start_file_name (str, optional): The name of the first file to rename +1. Defaults to "00" (If you want to start renaming at the second file (02, 03...), put "01")
             file_extension (str, optional): The extension of the files to rename and the extension of the renamed files. Defaults to ".jpg".
-            rename_format (str, optional): The format of the renamed files. Defaults to "0". Can be "00", "000", etc. (0=01, 00=001, etc.)
+            start_number (int, optional): The number of the first file to rename. Defaults to 0. (If you want to start renaming at the second file (02, 03...), put 2)
+            rename_format (str, optional): The format of the renamed files. Defaults to blank. Can be "0", "00", etc. (0=01, 00=001, etc.)
         """
-        self.__renamer(path, file_extension, start, rename_format)
-        sleep(3) # Wait 3 seconds before removing the "n_" (to avoid dupe bugs)
-        self.__remove_n(path, file_extension, start)
+        self.__renamer(path, file_extension, start_file_name, start_number, rename_format)
+        sleep(2) # Wait 2 seconds before removing the "n_" (to avoid dupe bugs)
+        self.__remove_n(path, file_extension, start_file_name)
 
 # tests
 if __name__ == '__main__':
